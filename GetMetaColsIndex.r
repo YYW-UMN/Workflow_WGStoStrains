5c3ae0501dcf82897dd57c2476f02379ac31398a
Yuanyuan Wang <38868191+YYW-UMN@users.noreply.github.com>
2020-04-06 03:13:28 -0500

    Delete GetMetaColsIndex.r

--- GetMetaColsIndex.r
+++
@@ -1,7 +0,0 @@
-GetMetaColsIndex <- function(gen){
-        cols <- seq(1,ncol(gen),1)
-        gt.cols <- grep('X',colnames(gen))
-        meta.cols <- setdiff(cols,gt.cols)
-        return(meta.cols)
-}
-
