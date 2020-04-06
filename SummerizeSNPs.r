1b6491fff73bf413cb597fe06011b6f8d979467f
Yuanyuan Wang <38868191+YYW-UMN@users.noreply.github.com>
2020-04-06 03:24:21 -0500

    Update SummerizeSNPs.r

--- SummerizeSNPs.r
+++ SummerizeSNPs.r
@@ -1,10 +1,10 @@
 
-farms.geno
+location.geno
 
-SummerizeSNPs <- function(farms.geno) {
+SummerizeSNPs <- function(location.geno) {
 
-	for (f in (1:length(farms.geno))){
-		geno <- farms.geno[[f]] 
+	for (f in (1:length(location.geno)){
+		geno <- location.geno[[f]] 
                 counts <- matrix(0,ncol = 4,nrow = ncol(geno))
                 for (s in (1:ncol(geno))){ 
                         counts[s,1] <- sum(geno[,s] == 1)+ sum(geno[,s] == 2)+ sum(geno[,s] == 3)+ sum(geno[,s] == 4) # hom 
@@ -32,35 +32,30 @@ SummerizeSNPs <- function(farms.geno) {
 		}
 	}
 
-	farm <- farms.geno
+	location <- location.geno
 	pos <- c()
-	for (n in (1:length(colnames(farms.geno)))){
-		p <- as.numeric(unlist(strsplit(colnames(farms.geno)[n],"X"))[2])
+	for (n in (1:length(colnames(location.geno)))){
+		p <- as.numeric(unlist(strsplit(colnames(location.geno)[n],"X"))[2])
 		pos <- append(pos,p)
 	}
-	farm$Date <- mapvalues(farm$Date,'unknown',999)
-	snp.count <- matrix(0, nrow = length(pos),ncol = length(unique(farm$Date)))
-	#sample.count <- matrix(0, nrow = length(pos),ncol = length(unique(farm$Date)))
+	location$Date <- mapvalues(location$Date,'unknown',999)
+	snp.count <- matrix(0, nrow = length(pos),ncol = length(unique(location$Date)))
+	#sample.count <- matrix(0, nrow = length(pos),ncol = length(unique(location$Date)))
 
-	colnames(snp.count) <- sort(unique(farm$Date))
+	colnames(snp.count) <- sort(unique(location$Date))
 	rownames(snp.count) <- colnames(geno)
 
-        sample.count <- matrix(0, nrow = length(pos),ncol = length(unique(farm$Date)))
-        colnames(sample.count) <- sort(unique(farm$Date))
+        sample.count <- matrix(0, nrow = length(pos),ncol = length(unique(location$Date)))
+        colnames(sample.count) <- sort(unique(location$Date))
         rownames(sample.count) <- colnames(geno)
 	
-	snp.list <- grep('X',colnames(farm))
-	date <- grep('Date',colnames(farm))
+	snp.list <- grep('X',colnames(location))
+	date <- grep('Date',colnames(location))
 	#snp.count <- cbind(pos,snps,snp.count)
 	#years <- as.numeric(colnames(snp.count))
-	dat <- farm[,c(date,start:ncol(farm))]
-	
-
-	for (snp in 1:nrow(sample.count)){ # rows are snps
-		
-			sample.count[snp,] <- aggregate(dat[,cols],by = list(Category = dat[,'Date']), FUN = length)[,2]
-		#non.zero.rows <- which(dat[,snp+1] > 0 & dat[,snp+1] < 998) 		
-	}
+	dat <- location[,c(date,start:ncol(location))]
+	return(dat)
+}
 
 
 
